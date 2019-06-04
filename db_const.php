fc0a7a2f846b3d73f67d4a20861ab90eb19f5864
gothiclemons <45483819+gothiclemons@users.noreply.github.com>
2019-06-04 11:47:28 +0100

    Add files via upload

---
+++ db_const.php
@@ -0,0 +1,7 @@
+<?php
+	# mysql db constants DB_HOST, DB_USER, DB_PASS, DB_NAME
+	const DB_HOST = 'localhost';
+	const DB_USER = 'id8610083_user';
+	const DB_PASS = 'password';
+	const DB_NAME = 'id8610083_php_mysql_login_systm';
+?>
